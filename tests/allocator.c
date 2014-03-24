3f00a360a400ee9f310c4cb95bc80c6a247ae516
Mohammed Sameer <msameer@foolab.org>
2014-03-24 02:51:49 +0200

    move the test to gstreamer check

--- tests/allocator.c
+++ tests/allocator.c
@@ -1,40 +1,81 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <gst/gst.h>
+#include <gst/check/gstcheck.h>
 #include "gstgralloc.h"
-#include <assert.h>
 
-int
-main(int argc, char *argv[]) {
-  gst_init(&argc, &argv);
-
+GST_START_TEST (test_create_destroy)
+{
   GstAllocator *allocator = gst_gralloc_allocator_new();
-  assert (allocator != NULL);
+
+  fail_unless(allocator != NULL);
 
   gst_object_unref(GST_OBJECT(allocator));
+}
+GST_END_TEST;
 
-  allocator = gst_gralloc_allocator_new();
+GST_START_TEST (test_create_alloc_destroy)
+{
+  GstAllocator *allocator = gst_gralloc_allocator_new();
+
+  fail_unless(allocator != NULL);
+
   GstMemory *mem = gst_gralloc_allocator_alloc (allocator, 640, 480, 0x32315659, GST_GRALLOC_USAGE_SW_READ_MASK | GST_GRALLOC_USAGE_SW_WRITE_MASK | GST_GRALLOC_USAGE_HW_TEXTURE);
 
-  assert(mem != NULL);
+  fail_unless(mem != NULL);
 
-  assert(gst_is_gralloc_memory(mem) == TRUE);
-  assert(GST_MEMORY_IS_READONLY(mem) == FALSE);
-  assert(GST_MEMORY_IS_NO_SHARE(mem) == TRUE);
-  assert(GST_MEMORY_IS_ZERO_PADDED(mem) == FALSE);
-  assert(GST_MEMORY_IS_ZERO_PREFIXED(mem) == FALSE);
-  assert(GST_MEMORY_IS_PHYSICALLY_CONTIGUOUS(mem) == FALSE);
-  assert(GST_MEMORY_IS_NOT_MAPPABLE(mem) == TRUE);
+  ASSERT_CAPS_REFCOUNT(allocator, "allocator", 2);
+  gst_memory_unref(mem);
+
+  gst_object_unref(GST_OBJECT(allocator));
+}
+GST_END_TEST;
+
+GST_START_TEST (test_memory)
+{
+  GstAllocator *allocator = gst_gralloc_allocator_new();
+
+  fail_unless(allocator != NULL);
+
+  GstMemory *mem = gst_gralloc_allocator_alloc (allocator, 640, 480, 0x32315659, GST_GRALLOC_USAGE_SW_READ_MASK | GST_GRALLOC_USAGE_SW_WRITE_MASK | GST_GRALLOC_USAGE_HW_TEXTURE);
+
+  fail_unless(mem != NULL);
+  fail_unless_equals_int (gst_is_gralloc_memory(mem), TRUE);
+  fail_unless_equals_int (GST_MEMORY_IS_READONLY(mem), FALSE);
+  fail_unless_equals_int (GST_MEMORY_IS_NO_SHARE(mem), TRUE);
+  fail_unless_equals_int (GST_MEMORY_IS_ZERO_PADDED(mem), FALSE);
+  fail_unless_equals_int (GST_MEMORY_IS_ZERO_PREFIXED(mem), FALSE);
+  fail_unless_equals_int (GST_MEMORY_IS_PHYSICALLY_CONTIGUOUS(mem), FALSE);
+  fail_unless_equals_int (GST_MEMORY_IS_NOT_MAPPABLE(mem), TRUE);
 
   gsize size, offset, maxsize;
   size = gst_memory_get_sizes (mem, &offset, &maxsize);
 
-  assert(size == -1);
-  assert(offset == 0);
-  assert(maxsize == -1);
+  fail_unless_equals_int (size, -1);
+  fail_unless_equals_int (offset, 0);
+  fail_unless_equals_int (maxsize, -1);
 
   gst_memory_unref(mem);
+
   gst_object_unref(GST_OBJECT(allocator));
-
-  gst_deinit();
-
-  return 0;
 }
+GST_END_TEST;
+
+static Suite *
+allocator_suite (void)
+{
+  Suite *s = suite_create ("gralloc allocator");
+  TCase *tc_chain = tcase_create ("general");
+
+  suite_add_tcase (s, tc_chain);
+
+  tcase_add_test (tc_chain, test_create_destroy);
+  tcase_add_test (tc_chain, test_create_alloc_destroy);
+  tcase_add_test (tc_chain, test_memory);
+
+  return s;
+}
+
+GST_CHECK_MAIN (allocator);
