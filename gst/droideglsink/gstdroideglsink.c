9156ca6014feae990bae72be69d1f57a155d038c
Mohammed Sameer <msameer@foolab.org>
2014-03-26 23:15:29 +0200

    Use gst_buffer_peek_memory instead of gst_buffer_get_memory

--- gst/droideglsink/gstdroideglsink.c
+++ gst/droideglsink/gstdroideglsink.c
@@ -385,14 +385,11 @@ gst_droideglsink_has_gralloc_memory (GstDroidEglSink * sink, GstBuffer * buffer)
   GST_DEBUG_OBJECT (sink, "examining %d memory items", num);
 
   for (x = 0; x < num; x++) {
-    GstMemory *mem = gst_buffer_get_memory (buffer, x);
+    GstMemory *mem = gst_buffer_peek_memory (buffer, x);
 
     if (mem && gst_memory_is_type (mem, GST_ALLOCATOR_GRALLOC)) {
-      gst_memory_unref (mem);
       return TRUE;
     }
-
-    gst_memory_unref (mem);
   }
 
   return FALSE;
