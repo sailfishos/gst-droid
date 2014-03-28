31ea9a0147b4e93eae417410843382f27e9f994a
Mohammed Sameer <msameer@foolab.org>
2014-03-29 00:09:26 +0200

    more skeleton code

--- gst/droidcodec/gstdroiddec.c
+++ gst/droidcodec/gstdroiddec.c
@@ -33,17 +33,199 @@ GST_DEBUG_CATEGORY_STATIC (gst_droid_dec_debug);
 #define GST_CAT_DEFAULT gst_droid_dec_debug
 
 static GstStaticPadTemplate gst_droiddec_src_template_factory =
-GST_STATIC_PAD_TEMPLATE ("src",
+GST_STATIC_PAD_TEMPLATE (GST_VIDEO_DECODER_SRC_NAME,
     GST_PAD_SRC,
     GST_PAD_ALWAYS,
     GST_STATIC_CAPS (GST_VIDEO_CAPS_MAKE_WITH_FEATURES
         (GST_CAPS_FEATURE_MEMORY_DROID_SURFACE, "{ENCODED, YV12}")));
 
 static GstStaticPadTemplate gst_droiddec_sink_template_factory =
-GST_STATIC_PAD_TEMPLATE ("sink",
+GST_STATIC_PAD_TEMPLATE (GST_VIDEO_DECODER_SINK_NAME,
     GST_PAD_SINK,
     GST_PAD_ALWAYS,
     GST_STATIC_CAPS_ANY);
+
+static void
+gst_droiddec_finalize (GObject * object)
+{
+  GstDroidDec *dec = GST_DROIDDEC (object);
+
+  GST_DEBUG_OBJECT (dec, "finalize");
+
+  // TODO:
+
+  G_OBJECT_CLASS (parent_class)->finalize (object);
+}
+
+static gboolean
+gst_droiddec_open (GstVideoDecoder * decoder)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "open");
+
+  // TODO:
+
+  return TRUE;
+}
+
+static gboolean
+gst_droiddec_close (GstVideoDecoder * decoder)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "close");
+
+
+  // TODO:
+
+  return TRUE;
+}
+
+static gboolean
+gst_droiddec_start (GstVideoDecoder * decoder)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "start");
+
+  // TODO:
+
+  return TRUE;
+}
+
+static gboolean
+gst_droiddec_stop (GstVideoDecoder * decoder)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "stop");
+
+  // TODO:
+
+  return TRUE;
+}
+
+static gboolean
+gst_droiddec_set_format (GstVideoDecoder * decoder, GstVideoCodecState * state)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "set format %" GST_PTR_FORMAT, state);
+
+  // TODO:
+
+  return TRUE;
+}
+
+static gboolean
+gst_droiddec_reset (GstVideoDecoder * decoder, gboolean hard)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "reset %d", hard);
+
+  // TODO:
+
+  return TRUE;
+}
+
+static GstFlowReturn
+gst_droiddec_finish (GstVideoDecoder * decoder)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "finish");
+
+  // TODO:
+
+  return GST_FLOW_OK;
+}
+
+static GstFlowReturn
+gst_droiddec_handle_frame (GstVideoDecoder * decoder,
+    GstVideoCodecFrame * frame)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "handle frame");
+
+  // TODO:
+
+  return GST_FLOW_OK;
+}
+
+static gboolean
+gst_droiddec_sink_event (GstVideoDecoder * decoder, GstEvent * event)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "sink event %" GST_PTR_FORMAT, event);
+
+  // TODO:
+
+  return TRUE;
+}
+
+static gboolean
+gst_droiddec_src_event (GstVideoDecoder * decoder, GstEvent * event)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "src event %" GST_PTR_FORMAT, event);
+
+  // TODO:
+
+  return TRUE;
+}
+
+static gboolean
+gst_droiddec_negotiate (GstVideoDecoder * decoder)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "negotiate");
+
+  // TODO:
+
+  return TRUE;
+}
+
+static gboolean
+gst_droiddec_decide_allocation (GstVideoDecoder * decoder, GstQuery * query)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "decide allocation %" GST_PTR_FORMAT, query);
+
+  // TODO:
+
+  return TRUE;
+}
+
+static gboolean
+gst_droiddec_propose_allocation (GstVideoDecoder * decoder, GstQuery * query)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "propose allocation %" GST_PTR_FORMAT, query);
+
+  // TODO:
+
+  return TRUE;
+}
+
+static gboolean
+gst_droiddec_flush (GstVideoDecoder * decoder)
+{
+  GstDroidDec *dec = GST_DROIDDEC (decoder);
+
+  GST_DEBUG_OBJECT (dec, "flush");
+
+  // TODO:
+
+  return TRUE;
+}
 
 static void
 gst_droiddec_init (GstDroidDec * dec)
@@ -54,10 +236,13 @@ gst_droiddec_init (GstDroidDec * dec)
 static void
 gst_droiddec_class_init (GstDroidDecClass * klass)
 {
+  GObjectClass *gobject_class;
   GstElementClass *gstelement_class;
-  // TODO:
+  GstVideoDecoderClass *gstvideodecoder_class;
 
+  gobject_class = (GObjectClass *) klass;
   gstelement_class = (GstElementClass *) klass;
+  gstvideodecoder_class = (GstVideoDecoderClass *) klass;
 
   gst_element_class_set_static_metadata (gstelement_class,
       "Video sink", "Decoder/Video/Device",
@@ -69,6 +254,26 @@ gst_droiddec_class_init (GstDroidDecClass * klass)
   gst_element_class_add_pad_template (gstelement_class,
       gst_static_pad_template_get (&gst_droiddec_src_template_factory));
 
+  gobject_class->finalize = gst_droiddec_finalize;
+  gstvideodecoder_class->open = GST_DEBUG_FUNCPTR (gst_droiddec_open);
+  gstvideodecoder_class->close = GST_DEBUG_FUNCPTR (gst_droiddec_close);
+  gstvideodecoder_class->start = GST_DEBUG_FUNCPTR (gst_droiddec_start);
+  gstvideodecoder_class->stop = GST_DEBUG_FUNCPTR (gst_droiddec_stop);
+  gstvideodecoder_class->set_format =
+      GST_DEBUG_FUNCPTR (gst_droiddec_set_format);
+  gstvideodecoder_class->reset = GST_DEBUG_FUNCPTR (gst_droiddec_reset);
+  gstvideodecoder_class->finish = GST_DEBUG_FUNCPTR (gst_droiddec_finish);
+  gstvideodecoder_class->handle_frame =
+      GST_DEBUG_FUNCPTR (gst_droiddec_handle_frame);
+  gstvideodecoder_class->sink_event =
+      GST_DEBUG_FUNCPTR (gst_droiddec_sink_event);
+  gstvideodecoder_class->src_event = GST_DEBUG_FUNCPTR (gst_droiddec_src_event);
+  gstvideodecoder_class->negotiate = GST_DEBUG_FUNCPTR (gst_droiddec_negotiate);
+  gstvideodecoder_class->decide_allocation =
+      GST_DEBUG_FUNCPTR (gst_droiddec_decide_allocation);
+  gstvideodecoder_class->propose_allocation =
+      GST_DEBUG_FUNCPTR (gst_droiddec_propose_allocation);
+  gstvideodecoder_class->flush = GST_DEBUG_FUNCPTR (gst_droiddec_flush);
 }
 
 static gboolean
