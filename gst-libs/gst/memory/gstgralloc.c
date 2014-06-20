de232c3f4bcde4e2ec3403492dcf04236991afc0
Mohammed Sameer <msameer@foolab.org>
2014-06-21 00:02:18 +0300

    Revert "droidmemory: first attempt at moving away from direct gralloc usage"
    This reverts commit fcd412f0a074162950446183e1dbc6c5a25f5447.

--- gst-libs/gst/memory/gstgralloc.c
+++ gst-libs/gst/memory/gstgralloc.c
@@ -25,7 +25,7 @@
 
 #include <gst/gst.h>
 #include "gstgralloc.h"
-#include <EGL/egl.h>
+#include <hardware/gralloc.h>
 #include <system/window.h>
 
 #define container_of(ptr, type, member) ({			\
@@ -39,15 +39,10 @@ typedef struct
 {
   GstAllocator parent;
 
-  EGLDisplay dpy;
-  EGLContext ctx;
+  gralloc_module_t *gralloc;
+  alloc_device_t *alloc;
+  GMutex mutex;
 
-    EGLBoolean (*eglHybrisCreateNativeBuffer) (EGLint width, EGLint height,
-      EGLint usage, EGLint format, EGLint * stride, EGLClientBuffer * buffer);
-  void (*eglHybrisReleaseNativeBuffer) (EGLClientBuffer buffer);
-    EGLBoolean (*eglHybrisLockNativeBuffer) (EGLClientBuffer buffer,
-      EGLint usage, EGLint l, EGLint t, EGLint w, EGLint h, void **vaddr);
-    EGLBoolean (*eglHybrisUnlockNativeBuffer) (EGLClientBuffer buffer);
 } GstGrallocAllocator;
 
 typedef struct
@@ -60,10 +55,7 @@ typedef struct
 {
   GstMemory mem;
 
-  struct ANativeWindowBuffer *remote;
-
-  void (*incRef) (struct android_native_base_t * base);
-  void (*decRef) (struct android_native_base_t * base);
+  struct ANativeWindowBuffer buff;
 
 } GstGrallocMemory;
 
@@ -86,15 +78,9 @@ incRef (struct android_native_base_t *base)
 {
   struct ANativeWindowBuffer *self =
       container_of (base, struct ANativeWindowBuffer, common);
-
-  GstGrallocMemory *mem = container_of (&self, GstGrallocMemory, remote);
+  GstGrallocMemory *mem = container_of (self, GstGrallocMemory, buff);
 
   gst_memory_ref (GST_MEMORY_CAST (mem));
-
-  GST_DEBUG ("ref %p", mem);
-
-  // TODO: crash
-  //  mem->incRef(base);
 }
 
 static void
@@ -102,15 +88,11 @@ decRef (struct android_native_base_t *base)
 {
   struct ANativeWindowBuffer *self =
       container_of (base, struct ANativeWindowBuffer, common);
-  GstGrallocMemory *mem = container_of (&self, GstGrallocMemory, remote);
+  GstGrallocMemory *mem = container_of (self, GstGrallocMemory, buff);
 
   gst_memory_unref (GST_MEMORY_CAST (mem));
-
-  GST_DEBUG ("unref %p", mem);
-
-  // TODO: crash
-  //  mem->decRef(base);
 }
+
 
 GstAllocator *
 gst_gralloc_allocator_new (void)
@@ -124,40 +106,13 @@ gst_gralloc_allocator_new (void)
 static void
 gralloc_mem_allocator_init (GstGrallocAllocator * allocator)
 {
-  // TODO: error checking
-  EGLConfig config;
-  EGLint num_config;
-  static EGLint const attribute_list[] = {
-    EGL_RED_SIZE, 1,
-    EGL_GREEN_SIZE, 1,
-    EGL_BLUE_SIZE, 1,
-    EGL_NONE
-  };
-
   GstAllocator *alloc = GST_ALLOCATOR_CAST (allocator);
 
   GST_DEBUG_OBJECT (alloc, "init");
 
-  allocator->dpy = eglGetDisplay (EGL_DEFAULT_DISPLAY);
-
-  eglInitialize (allocator->dpy, NULL, NULL);
-  eglChooseConfig (allocator->dpy, attribute_list, &config, 1, &num_config);
-
-  allocator->ctx =
-      eglCreateContext (allocator->dpy, config, EGL_NO_CONTEXT, NULL);
-
-  *(void **) &allocator->eglHybrisCreateNativeBuffer =
-      eglGetProcAddress ("eglHybrisCreateNativeBuffer");
-
-  *(void **) &allocator->eglHybrisReleaseNativeBuffer =
-      eglGetProcAddress ("eglHybrisReleaseNativeBuffer");
-
-  *(void **) &allocator->eglHybrisLockNativeBuffer =
-      eglGetProcAddress ("eglHybrisUnlockNativeBuffer");
-
-  *(void **) &allocator->eglHybrisUnlockNativeBuffer =
-      eglGetProcAddress ("eglHybrisUnlockNativeBuffer");
-
+  allocator->gralloc = NULL;
+  allocator->alloc = NULL;
+  g_mutex_init (&allocator->mutex);
   alloc->mem_type = GST_ALLOCATOR_GRALLOC;
 
   alloc->mem_map = NULL;
@@ -176,9 +131,11 @@ gst_gralloc_allocator_finalize (GObject * obj)
 
   GST_DEBUG_OBJECT (alloc, "finalize");
 
-  if (alloc->ctx) {
-    eglDestroyContext (alloc->dpy, alloc->ctx);
+  if (alloc->alloc) {
+    gralloc_close (alloc->alloc);
   }
+
+  g_mutex_clear (&alloc->mutex);
 
   G_OBJECT_CLASS (parent_class)->finalize (obj);
 }
@@ -200,6 +157,7 @@ gst_gralloc_allocator_alloc (GstAllocator * allocator, gint width, gint height,
     int format, int usage)
 {
   GstGrallocAllocator *alloc;
+  int err;
   GstGrallocMemory *mem;
   int stride;
 
@@ -210,41 +168,50 @@ gst_gralloc_allocator_alloc (GstAllocator * allocator, gint width, gint height,
 
   alloc = GST_GRALLOC_ALLOCATOR (allocator);
 
-  if (!alloc->dpy) {
-    GST_ERROR ("no display");
-    return NULL;
-  }
+  g_mutex_lock (&alloc->mutex);
+  if (!alloc->gralloc) {
+    err =
+        hw_get_module (GRALLOC_HARDWARE_MODULE_ID,
+        (const hw_module_t **) &alloc->gralloc);
+    if (!alloc->gralloc) {
+      GST_ERROR ("failed to initialize gralloc: %d", err);
+      g_mutex_unlock (&alloc->mutex);
+      return NULL;
+    }
 
-  if (!alloc->ctx) {
-    GST_ERROR ("no context");
-    return NULL;
-  }
-
-  if (!alloc->eglHybrisCreateNativeBuffer) {
-    GST_ERROR ("no eglHybrisCreateNativeBuffer");
-    return NULL;
-  }
-
-  if (!alloc->eglHybrisReleaseNativeBuffer) {
-    GST_ERROR ("no eglHybrisReleaseNativeBuffer");
-    return NULL;
+    err = gralloc_open ((const hw_module_t *) alloc->gralloc, &alloc->alloc);
+    if (err) {
+      GST_ERROR ("failed to open gralloc: %d", err);
+      alloc->gralloc = NULL;
+      alloc->alloc = NULL;
+      g_mutex_unlock (&alloc->mutex);
+      return NULL;
+    }
   }
 
   /* Now we are ready to serve */
   mem = g_slice_new0 (GstGrallocMemory);
 
-  if (!alloc->eglHybrisCreateNativeBuffer (width, height, usage, format,
-          &stride, (EGLClientBuffer *) & mem->remote)) {
-    GST_ERROR ("Failed to allocate buffer");
+  err = alloc->alloc->alloc (alloc->alloc, width, height,
+      format, usage, &mem->buff.handle, &stride);
+  if (err) {
+    GST_ERROR ("Failed to allocate buffer handle");
     g_slice_free (GstGrallocMemory, mem);
+    g_mutex_unlock (&alloc->mutex);
     return NULL;
   }
 
-  mem->incRef = mem->remote->common.incRef;
-  mem->decRef = mem->remote->common.decRef;
+  mem->buff.width = width;
+  mem->buff.height = height;
+  mem->buff.stride = stride;
+  mem->buff.format = format;
+  mem->buff.usage = usage;
+  mem->buff.common.magic = ANDROID_NATIVE_BUFFER_MAGIC;
+  mem->buff.common.version = sizeof (struct ANativeWindowBuffer);
+  mem->buff.common.incRef = incRef;
+  mem->buff.common.decRef = decRef;
 
-  mem->remote->common.incRef = incRef;
-  mem->remote->common.decRef = decRef;
+  g_mutex_unlock (&alloc->mutex);
 
   gst_memory_init (GST_MEMORY_CAST (mem),
       GST_MEMORY_FLAG_NO_SHARE | GST_MEMORY_FLAG_NOT_MAPPABLE, allocator, NULL,
@@ -260,12 +227,9 @@ gst_gralloc_allocator_wrap (GstAllocator * allocator, gint width, gint height,
     int usage, guint8 * data, gsize size, GstVideoFormat format)
 {
   GstGrallocAllocator *alloc;
-  EGLBoolean res;
-  GstMemory *mem;
+  int err;
   void *addr = NULL;
   int hal_format = 0;
-  int lock_usage =
-      GST_GRALLOC_USAGE_SW_READ_NEVER | GST_GRALLOC_USAGE_SW_WRITE_RARELY;
 
   if (!GST_IS_GRALLOC_ALLOCATOR (allocator)) {
     return NULL;
@@ -273,23 +237,13 @@ gst_gralloc_allocator_wrap (GstAllocator * allocator, gint width, gint height,
 
   alloc = GST_GRALLOC_ALLOCATOR (allocator);
 
-  if (!alloc->eglHybrisLockNativeBuffer) {
-    GST_ERROR ("no eglHybrisLockNativeBuffer");
-    return NULL;
-  }
-
-  if (!alloc->eglHybrisUnlockNativeBuffer) {
-    GST_ERROR ("no eglHybrisUnlockNativeBuffer");
-    return NULL;
-  }
-
   hal_format = gst_gralloc_gst_to_hal (format);
 
   if (hal_format == 0) {
     return NULL;
   }
 
-  mem = gst_gralloc_allocator_alloc (allocator, width, height,
+  GstMemory *mem = gst_gralloc_allocator_alloc (allocator, width, height,
       hal_format, usage);
 
   if (!mem) {
@@ -297,12 +251,14 @@ gst_gralloc_allocator_wrap (GstAllocator * allocator, gint width, gint height,
   }
 
   /* lock */
-  res =
-      alloc->eglHybrisLockNativeBuffer (((GstGrallocMemory *) mem)->remote,
-      lock_usage, 0, 0, width, height, &addr);
-  if (!res) {
+  err =
+      alloc->gralloc->lock (alloc->gralloc,
+      ((GstGrallocMemory *) mem)->buff.handle,
+      GRALLOC_USAGE_SW_READ_NEVER | GRALLOC_USAGE_SW_WRITE_RARELY, 0, 0, width,
+      height, &addr);
+  if (err != 0) {
     gst_memory_unref (mem);
-    GST_ERROR ("failed to lock buffer");
+    GST_ERROR ("failed to lock the buffer: %d", err);
     return NULL;
   }
 
@@ -310,10 +266,12 @@ gst_gralloc_allocator_wrap (GstAllocator * allocator, gint width, gint height,
   memcpy (addr, data, size);
 
   /* unlock */
-  res = alloc->eglHybrisUnlockNativeBuffer (((GstGrallocMemory *) mem)->remote);
-  if (!res) {
+  err =
+      alloc->gralloc->unlock (alloc->gralloc,
+      ((GstGrallocMemory *) mem)->buff.handle);
+  if (err != 0) {
     gst_memory_unref (mem);
-    GST_ERROR ("failed to unlock buffer");
+    GST_ERROR ("failed to unlock the buffer: %d", err);
     return NULL;
   }
 
@@ -346,13 +304,14 @@ gst_gralloc_mem_copy (GstMemory * mem, gssize offset, gssize size)
 static void
 gst_gralloc_allocator_free (GstAllocator * allocator, GstMemory * mem)
 {
+  GstGrallocMemory *m = (GstGrallocMemory *) mem;
   GstGrallocAllocator *alloc = GST_GRALLOC_ALLOCATOR (allocator);
 
-  GST_DEBUG_OBJECT (alloc, "free %p", mem);
+  GST_DEBUG_OBJECT (alloc, "free %p", m);
 
-  alloc->eglHybrisReleaseNativeBuffer (((GstGrallocMemory *) mem)->remote);
+  alloc->alloc->free (alloc->alloc, m->buff.handle);
 
-  g_slice_free (GstGrallocMemory, (GstGrallocMemory *) mem);
+  g_slice_free (GstGrallocMemory, m);
 }
 
 struct ANativeWindowBuffer *
@@ -362,7 +321,7 @@ gst_memory_get_native_buffer (GstMemory * mem)
     return NULL;
   }
 
-  return ((GstGrallocMemory *) mem)->remote;
+  return &((GstGrallocMemory *) mem)->buff;
 }
 
 GstVideoFormat
