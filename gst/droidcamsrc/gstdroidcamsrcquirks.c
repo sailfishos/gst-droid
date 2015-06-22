cefb820088c8b47fb0ee4b8c8cb38d67d92be071
Mohammed Sameer <msameer@foolab.org>
2015-06-22 05:03:07 +0300

    [droidcamsrc] Add a new quirk type that uses gst_droidcamsrc_dev_send_command() instead of normal properties

--- gst/droidcamsrc/gstdroidcamsrcquirks.c
+++ gst/droidcamsrc/gstdroidcamsrcquirks.c
@@ -28,6 +28,33 @@
 #include "gstdroidcamsrc.h"
 #include "gstdroidcamsrcparams.h"
 
+/*
+ * Quirks are non-standard functionality which can be enabled or disabled
+ * by either setting a CameraParameters parameter or by doing a send_message()
+ * with a specific command and arguments.
+ * Quirks are read from $(sysconfdir)/gst-droid/gstdroidcamsrcquirks.conf
+ *
+ * NOTE: if type is not defined then we assume it's a property to keep backward compatibility
+ *
+ * Format of a property quirk definition:
+ * [quirk-id]
+ * type=property
+ * prop=<property name>
+ * on=<value to turn on>
+ * off=<value to turn off>
+ * direction=<-1 = all devices or a camera device id>
+ *
+ * Format of a command quirk definition:
+ * [quirk-id]
+ * type=command
+ * command_enable=<command value used for enabling>
+ * command_disable=<command value used for disabling>
+ * arg1_enable=<value of the first argument used for enabling>
+ * arg2_enable=<value of the second argument used for enabling>
+ * arg1_disable=<value of the first argument used for disabling>
+ * arg2_disable=<value of the second argument used for disabling>
+ * direction=<-1 = all devices or a camera device id>
+ */
 #define CHECK_ERROR(e,s,p)						\
   if (e) {								\
     GST_WARNING ("failed to load %s for %s: %s", p, s, err->message);	\
@@ -38,6 +65,14 @@
 GST_DEBUG_CATEGORY_EXTERN (gst_droid_camsrc_debug);
 #define GST_CAT_DEFAULT gst_droid_camsrc_debug
 
+typedef enum _GstDRoidCamSrcQuirkType GstDRoidCamSrcQuirkType;
+
+enum _GstDRoidCamSrcQuirkType
+{
+  GST_DROID_CAM_SRC_QUIRK_PROPERTY = 0,
+  GST_DROID_CAM_SRC_QUIRK_COMMAND = 1,
+};
+
 struct _GstDroidCamSrcQuirks
 {
   GList *quirks;
@@ -47,9 +82,21 @@ struct _GstDroidCamSrcQuirk
 {
   gint direction;
   gchar *id;
+
+  GstDRoidCamSrcQuirkType type;
+
+  /* property quirk */
   gchar *prop;
   gchar *on;
   gchar *off;
+
+  /* command quirk */
+  gint command_enable;
+  gint command_disable;
+  gint arg1_enable;
+  gint arg2_enable;
+  gint arg1_disable;
+  gint arg2_disable;
 };
 
 void
@@ -83,25 +130,64 @@ gst_droidcamsrc_quirk_new (GKeyFile * file, const gchar * group)
 {
   GstDroidCamSrcQuirk *quirk = g_slice_new0 (GstDroidCamSrcQuirk);
   GError *err = NULL;
+  gchar *type = NULL;
 
+  /* common properties first */
   quirk->id = g_strdup (group);
-
-  quirk->prop = g_key_file_get_value (file, group, "prop", &err);
-  CHECK_ERROR (err, group, "prop");
-
-  quirk->on = g_key_file_get_value (file, group, "on", &err);
-  CHECK_ERROR (err, group, "on");
-
-  quirk->off = g_key_file_get_value (file, group, "off", &err);
-  CHECK_ERROR (err, group, "off");
 
   quirk->direction = g_key_file_get_integer (file, group, "direction", &err);
   CHECK_ERROR (err, group, "direction");
 
-  if (!quirk->prop || !quirk->on || !quirk->off) {
-    GST_WARNING ("incomplete quirk definition for %s", group);
-    gst_droidcamsrc_quirk_free (quirk);
-    quirk = NULL;
+  type = g_key_file_get_value (file, group, "type", &err);
+  CHECK_ERROR (err, group, "type");
+
+  quirk->type = GST_DROID_CAM_SRC_QUIRK_PROPERTY;
+
+  if (!g_strcmp0 (type, "command")) {
+    quirk->type = GST_DROID_CAM_SRC_QUIRK_COMMAND;
+  }
+
+  if (type) {
+    g_free (type);
+    type = NULL;
+  }
+
+  if (quirk->type == GST_DROID_CAM_SRC_QUIRK_PROPERTY) {
+    quirk->prop = g_key_file_get_value (file, group, "prop", &err);
+    CHECK_ERROR (err, group, "prop");
+
+    quirk->on = g_key_file_get_value (file, group, "on", &err);
+    CHECK_ERROR (err, group, "on");
+
+    quirk->off = g_key_file_get_value (file, group, "off", &err);
+    CHECK_ERROR (err, group, "off");
+
+    if (!quirk->prop || !quirk->on || !quirk->off) {
+      GST_WARNING ("incomplete quirk definition for %s", group);
+      gst_droidcamsrc_quirk_free (quirk);
+      quirk = NULL;
+    }
+  } else {
+
+    quirk->command_enable =
+        g_key_file_get_integer (file, group, "command_enable", &err);
+    CHECK_ERROR (err, group, "command_enable");
+
+    quirk->command_disable =
+        g_key_file_get_integer (file, group, "command_disable", &err);
+    CHECK_ERROR (err, group, "command_disable");
+    quirk->arg1_enable =
+        g_key_file_get_integer (file, group, "arg1_enable", &err);
+    CHECK_ERROR (err, group, "arg1_enable");
+    quirk->arg2_enable =
+        g_key_file_get_integer (file, group, "arg2_enable", &err);
+    CHECK_ERROR (err, group, "arg2_enable");
+    quirk->arg1_disable =
+        g_key_file_get_integer (file, group, "arg1_disable", &err);
+    CHECK_ERROR (err, group, "arg1_disable");
+    quirk->arg2_disable =
+        g_key_file_get_integer (file, group, "arg2_disable", &err);
+    CHECK_ERROR (err, group, "arg2_disable");
   }
 
   return quirk;
@@ -176,12 +262,23 @@ gst_droidcamsrc_quirks_apply (GstDroidCamSrcQuirks * quirks,
   if (quirk->direction == direction || quirk->direction == -1) {
     if (enable) {
       GST_DEBUG_OBJECT (src, "enabling %s", quirk_id);
-      gst_droidcamsrc_params_set_string (src->dev->params,
-          quirk->prop, quirk->on);
+
+      if (quirk->type == GST_DROID_CAM_SRC_QUIRK_PROPERTY) {
+        gst_droidcamsrc_params_set_string (src->dev->params,
+            quirk->prop, quirk->on);
+      } else {
+        gst_droidcamsrc_dev_send_command (src->dev, quirk->command_enable,
+            quirk->arg1_enable, quirk->arg2_enable);
+      }
     } else {
       GST_DEBUG_OBJECT (src, "disabling %s", quirk_id);
-      gst_droidcamsrc_params_set_string (src->dev->params,
-          quirk->prop, quirk->off);
+      if (quirk->type == GST_DROID_CAM_SRC_QUIRK_PROPERTY) {
+        gst_droidcamsrc_params_set_string (src->dev->params,
+            quirk->prop, quirk->off);
+      } else {
+        gst_droidcamsrc_dev_send_command (src->dev, quirk->command_disable,
+            quirk->arg1_disable, quirk->arg2_disable);
+      }
     }
   }
 }
